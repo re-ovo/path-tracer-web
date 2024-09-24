85dd096fb986bc99a326f9d2db03cded5b1ec2f2
RE <qq1609403959@outlook.com>
2024-09-25 00:18:11 +0800

    nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NProgress from "nprogress";
+import 'nprogress/nprogress.css'
+
+const nProgress = NProgress.configure({ showSpinner: false })
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/works',
-      name: 'works',
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: 'first-week',
-          name: 'first-week',
-          component: () => import('../views/works/first-week.vue')
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/works',
+            name: 'works',
+            children: [
+                {
+                    path: 'first-week',
+                    name: 'first-week',
+                    component: () => import('../views/works/first-week.vue')
+                }
+            ]
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: '404',
+            component: () => import('../views/404.vue')
         }
-      ]
-    },
-    {
-      path: '/:catchAll(.*)',
-      name: '404',
-      component: () => import('../views/404.vue')
-    }
-  ]
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    nProgress.start()
+    next()
+})
+
+router.afterEach(() => {
+    nProgress.done()
 })
 
 export default router
--- src/views/works/first-week.vue
+++ src/views/works/first-week.vue
@@ -30,17 +30,14 @@ const draw = (ctx: CanvasRenderingContext2D, width: number, height: number) => {
       const u = i / width
       const v = j / height
 
-      const ray = new Ray(new Vec3(0, 0, 0), new Vec3(u, v, 1).normalize())
-      const color = rayTrace(ray, 0)
-      drawPixel(ctx, i, j, color)
+      // TODO
     }
   }
 }
 
 const rayTrace = (ray: Ray, depth: number): Vec3 => {
   if (depth > MAX_LIGHT_BOUNCES) return new Vec3(0, 0, 0)
-
-  const t = 0.5 * (ray.getDirection().y + 1)
-  return Vec3.lerp(new Vec3(1, 1, 1), new Vec3(0.5, 0.7, 1.0), t)
+  // TODO
+  return new Vec3(0, 0, 0)
 }
 </script>
